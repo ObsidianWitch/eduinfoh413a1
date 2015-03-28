cf1851a0e34af730debb67765a46570f407dc003
Obside <obsiagae@gmail.com>
2015-03-28 16:16:51 +0100

    main.c code formatting

--- src/main.c
+++ src/main.c
@@ -32,110 +32,124 @@
 char *FileName;
 
 void readOpts(int argc, char **argv) {
-  char opt;
+    char opt;
 
-  FileName = NULL;
-  while ( (opt = getopt(argc, argv, "i:")) > 0 )  
-      switch (opt) {
-	  case 'i': /* Instance file */
-	      FileName = (char *)malloc(strlen(optarg)+1);
-	      strncpy(FileName, optarg, strlen(optarg));
-	      break;
-	  default:
-	      fprintf(stderr, "Option %c not managed.\n", opt);
-      }
-    
-  if ( !FileName ) {
-    printf("No instance file provided (use -i <instance_name>). Exiting.\n");
-    exit(1);
-  }
+    FileName = NULL;
+    while ( (opt = getopt(argc, argv, "i:")) > 0 ) {
+        switch (opt) {
+            case 'i': /* Instance file */
+                FileName = (char *)malloc(strlen(optarg)+1);
+                strncpy(FileName, optarg, strlen(optarg));
+                break;
+            
+            default:
+                fprintf(stderr, "Option %c not managed.\n", opt);
+        }
+    }
+
+    if ( !FileName ) {
+        printf("No instance file provided (use -i <instance_name>). Exiting.\n");
+        exit(1);
+    }
 }
 
 
 
-int main (int argc, char **argv) 
-{
-  long int i,j;
-  long int *currentSolution;
-  int cost, newCost, temp, firstRandomPosition, secondRandomPosition;
+int main (int argc, char **argv) {
+    long int i,j;
+    long int *currentSolution;
+    int cost, newCost, temp, firstRandomPosition, secondRandomPosition;
 
-  /* Do not buffer output */
-  setbuf(stdout,NULL);
-  setbuf(stderr,NULL);
-  
-  if (argc < 2) {
-    printf("No instance file provided (use -i <instance_name>). Exiting.\n");
-    exit(1);
-  }
-  
-  /* Read parameters */
-  readOpts(argc, argv);
+    /* Do not buffer output */
+    setbuf(stdout,NULL);
+    setbuf(stderr,NULL);
 
-  /* Read instance file */
-  CostMat = readInstance(FileName);
-  printf("Data have been read from instance file. Size of instance = %ld.\n\n", PSize);
+    if (argc < 2) {
+        printf("No instance file provided (use -i <instance_name>). Exiting.\n");
+        exit(1);
+    }
 
-  /* initialize random number generator, deterministically based on instance.
-   * To do this we simply set the seed to the sum of elements in the matrix, so it is constant per-instance,
-   but (most likely) varies between instances */
-  Seed = (long int) 0;
-    for (i=0; i < PSize; ++i)
-      for (j=0; j < PSize; ++j)
-        Seed += (long int) CostMat[i][j];
-  printf("Seed used to initialize RNG: %ld.\n\n", Seed);
-  
-  /* starts time measurement */
-  start_timers();
+    /* Read parameters */
+    readOpts(argc, argv);
 
-  /* A solution is just a vector of int with the same size as the instance */
-  currentSolution = (long int *)malloc(PSize * sizeof(long int));
+    /* Read instance file */
+    CostMat = readInstance(FileName);
+    printf("Data have been read from instance file. Size of instance = %ld.\n\n",
+        PSize);
 
-  /* Create an initial random solution. 
-     The only constraint is that it should always be a permutation */
-  createRandomSolution(currentSolution);
+    /* initialize random number generator, deterministically based on instance.
+     * To do this we simply set the seed to the sum of elements in the matrix,
+     * so it is constant per-instance, but (most likely) varies between
+     * instances. */
+    Seed = (long int) 0;
+    for (i = 0 ; i < PSize ; ++i) {
+        for (j = 0 ; j < PSize ; ++j) {
+            Seed += (long int) CostMat[i][j];
+        }
+    }
+    printf("Seed used to initialize RNG: %ld.\n\n", Seed);
 
-  /* Print solution */
-  printf("Initial solution:\n");
-  for (j=0; j < PSize; j++) 
-    printf(" %ld", currentSolution[j]);
-  printf("\n");
+    /* starts time measurement */
+    start_timers();
 
-  /* Compute cost of solution and print it */
-  cost = computeCost(currentSolution);
-  printf("Cost of this initial solution: %d\n\n", cost);
+    /* A solution is just a vector of int with the same size as the instance */
+    currentSolution = (long int *)malloc(PSize * sizeof(long int));
 
-  /* Example: apply an exchange operation of two elements at random position */
-  firstRandomPosition = randInt(0,PSize-1);
-  // Ensure second position is different from first one:
-  secondRandomPosition = firstRandomPosition + randInt(1,(PSize-2));
-  if (secondRandomPosition >= PSize)
-    secondRandomPosition -= PSize;
+    /* Create an initial random solution. The only constraint is that it should
+     * always be a permutation */
+    createRandomSolution(currentSolution);
 
-  printf("Two positions exchanged: %d and %d. ", firstRandomPosition, secondRandomPosition);
+    /* Print solution */
+    printf("Initial solution:\n");
+    for (j=0; j < PSize; j++) {
+        printf(" %ld", currentSolution[j]);
+    }
+    printf("\n");
 
-  temp = currentSolution[firstRandomPosition];
-  currentSolution[firstRandomPosition] = currentSolution[secondRandomPosition];
-  currentSolution[secondRandomPosition] = temp;
+    /* Compute cost of solution and print it */
+    cost = computeCost(currentSolution);
+    printf("Cost of this initial solution: %d\n\n", cost);
 
-  printf("Solution after exchange:\n");
-  for (j=0; j < PSize; j++) 
-    printf(" %ld", currentSolution[j]);
-  printf("\n");
+    // Example: apply an exchange operation of two elements at random position
+    firstRandomPosition = randInt(0,PSize-1);
+    // Ensure second position is different from first one:
+    secondRandomPosition = firstRandomPosition + randInt(1,(PSize-2));
+    if (secondRandomPosition >= PSize) {
+        secondRandomPosition -= PSize;
+    }
 
-  /* Recompute cost of solution after the exchange move */
-  /* There are some more efficient way to do this, instead of recomputing everything... */
-  newCost = computeCost(currentSolution);
-  printf("Cost of this solution after applying the exchange move: %d\n", newCost);
+    printf("Two positions exchanged: %d and %d. ", firstRandomPosition,
+        secondRandomPosition);
 
-  if (newCost == cost)
-    printf("Second solution is as good as first one\n");
-  else if (newCost > cost)
-    printf("Second solution is better than first one\n");
-  else
-    printf("Second solution is worse than first one\n");
+    temp = currentSolution[firstRandomPosition];
+    currentSolution[firstRandomPosition] = currentSolution[secondRandomPosition];
+    currentSolution[secondRandomPosition] = temp;
 
-  printf("Time elapsed since we started the timer: %g\n\n", elapsed_time(VIRTUAL));
+    printf("Solution after exchange:\n");
+    for (j=0; j < PSize; j++) {
+        printf(" %ld", currentSolution[j]);
+    }
+    printf("\n");
 
+    /* Recompute cost of solution after the exchange move
+     * TODO There are some more efficient way to do this, instead of recomputing
+     * everything... */
+    newCost = computeCost(currentSolution);
+    printf("Cost of this solution after applying the exchange move: %d\n",
+        newCost);
 
-  return 0;
+    if (newCost == cost) {
+        printf("Second solution is as good as first one\n");
+    }
+    else if (newCost > cost) {
+        printf("Second solution is better than first one\n");
+    }
+    else {
+        printf("Second solution is worse than first one\n");
+    }
+
+    printf("Time elapsed since we started the timer: %g\n\n",
+        elapsed_time(VIRTUAL));
+
+    return 0;
 }
