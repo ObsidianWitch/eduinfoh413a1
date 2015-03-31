a4001eacf501bbe073f2f2619f2cbc2bb0299ed8
Obside <obsiagae@gmail.com>
2015-03-31 11:01:59 +0200

    basic help message

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
+#include <iostream>
 #include <cstdlib>
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <instance_file>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
