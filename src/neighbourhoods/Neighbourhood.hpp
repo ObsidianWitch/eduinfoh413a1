66cb7dd4c5f2a01f905cee966ccc723cec1ebfa5
Obside <obsiagae@gmail.com>
2015-04-03 23:28:46 +0200

    Neighbourhood class is not abstract anymore

--- src/neighbourhoods/Neighbourhood.hpp
+++ src/neighbourhoods/Neighbourhood.hpp
@@ -3,14 +3,24 @@
 
 class Neighbourhood {
 public:
-    virtual void start() = 0;
-    virtual bool is_valid() = 0;
-    virtual void next() = 0;
+    static const unsigned TRANSPOSE = 1;
+    static const unsigned EXCHANGE  = 2;
+    static const unsigned INSERTION = 3;
+
+    Neighbourhood(unsigned size, unsigned step) :
+        size_(size), step_(step)
+    {}
+
+    void start() { i_ = 0; };
+    bool is_valid() { i_ < size_; }
+    void next() { i_ += step_; }
 
     operator unsigned() const { return i_; }
 
 protected:
     unsigned i_;
+    unsigned size_;
+    unsigned step_;
 };
 
 #endif // NEIGHBOURHOOD_HPP
