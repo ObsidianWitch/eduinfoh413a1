62d4aac56efbc4fa05c6996911b663e5ad3c6290
Obside <obsiagae@gmail.com>
2015-04-04 01:20:25 +0200

    load_instance test

---
+++ src/tests/load_instance.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/detail/matrix_assign.hpp>
+#include <boost/numeric/ublas/io.hpp>
+namespace ublas = boost::numeric::ublas;
+
+#include "Instance.hpp"
+
+int main() {
+    ublas::matrix<int> testMatrix(5, 5);
+    for (unsigned k = 0 ; k < 25 ; k++) {
+        unsigned i = k / 5;
+        unsigned j = k % 5;
+        testMatrix(i,j) = k;
+    }
+    std::cout << "test matrix" << std::endl
+              << testMatrix << std::endl;
+    
+    Instance instance("../instances/test");
+    std::cout << "instance matrix" << std::endl
+              << instance.matrix() << std::endl;
+    
+    assert(ublas::detail::equals(testMatrix, instance.matrix(), 1.e-6, 0.));
+    
+    return EXIT_SUCCESS;
+}
