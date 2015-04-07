8f8f14c16f566a014119c1c4ceb5c182de62d8fd
Obside <obsiagae@gmail.com>
2015-04-07 08:03:46 +0200

    VND

--- src/main_VND.cpp
+++ src/main_VND.cpp
@@ -5,7 +5,7 @@
 #include "rules/Initialization/CWInitialization.hpp"
 #include "rules/Pivoting/FirstImprovement.hpp"
 #include "rules/RulesFactory.hpp"
-#include "IterativeImprovement.hpp"
+#include "VariableNeighbourhoodDescent.hpp"
 #include "GlobalArgsVND.hpp"
 #include "Instance.hpp"
 
@@ -27,24 +27,24 @@ int main(int argc, char *argv[]) {
     // Algorithm dependencies
     Instance instance(g.filePath.c_str());
     CWInitialization initialization(instance);
-    // FIXME FirstImprovement improvement(instance);
-    Neighbourhood** neighbourhood = RulesFactory::getVNDNeighbourhood(
+    FirstImprovement improvement(instance);
+    Neighbourhood** neighbourhoods = RulesFactory::getVNDNeighbourhood(
         g.neighbourhood, instance.size()
     );
     
     // VND algorithm
-    /* FIXME VariableNeighbourhoodDescent vnd(instance, initialization, improvement,
-        neighbourhood); */
+    VariableNeighbourhoodDescent vnd(instance, initialization, improvement,
+        neighbourhoods);
     auto start = high_resolution_clock::now();
-    // FIXME vnd.run();
+    vnd.run();
     duration<double> timeElapsed = high_resolution_clock::now() - start;
     std::cout << "Time elapsed: " << timeElapsed.count() << " s" << std::endl;
     
     // Memory handling
-    for (unsigned i = 0 ; i < 3 ; i++) {
-        delete neighbourhood[i];
+    for (unsigned i = 0 ; i < 3 ; i++) {ç
+        delete neighbourhoods[i];
     }
-    delete[] neighbourhood;
+    delete[] neighbourhoods;
 
     return EXIT_SUCCESS;
 }
