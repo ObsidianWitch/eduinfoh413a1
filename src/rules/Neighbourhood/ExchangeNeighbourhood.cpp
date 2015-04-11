f4b9f2d718755ee27b978d471be54c5beb079b56
Obside <obsiagae@gmail.com>
2015-04-11 18:34:30 +0200

    ExchangeNeighbourhood::delta()

--- src/rules/Neighbourhood/ExchangeNeighbourhood.cpp
+++ src/rules/Neighbourhood/ExchangeNeighbourhood.cpp
@@ -27,3 +27,47 @@ Permutation ExchangeNeighbourhood::apply(const Permutation& p1) {
     p2.permute(p_.first, p_.second);
     return p2;
 }
+
+long int ExchangeNeighbourhood::delta(const Matrix& matrix,
+    const Permutation& oldP, const Permutation& newP) const
+{
+    long int deltaOldScore = 0;
+    long int deltaNewScore = 0;
+    long int first = p_.first;
+    long int second = p_.second;
+    
+    for (unsigned k = 0 ; k < size_ ; k++) {
+        unsigned oldPk = oldP[k],
+                 newPk = newP[k];
+        unsigned oldPfirst = oldP[first],
+                 newPfirst = newP[first];
+        unsigned oldPsecond = oldP[second],
+                 newPsecond = newP[second];
+        
+        if (first > k) {
+            deltaOldScore += matrix[oldPk][oldPfirst];
+            deltaNewScore += matrix[newPk][newPfirst];
+        }
+        
+        if (second > k) {
+            deltaOldScore += matrix[oldPk][oldPsecond];
+            deltaNewScore += matrix[newPk][newPsecond];
+        }
+        
+        if (first < k) {
+            if (k != first && k != second) {
+                deltaOldScore += matrix[oldPfirst][oldPk];
+                deltaNewScore += matrix[newPfirst][newPk];
+            }
+        }
+        
+        if (second < k) {
+            if (k != first && k != second) {
+                deltaOldScore += matrix[oldPsecond][oldPk];
+                deltaNewScore += matrix[newPsecond][newPk];
+            }
+        }
+    }
+    
+    return deltaNewScore - deltaOldScore;
+}
